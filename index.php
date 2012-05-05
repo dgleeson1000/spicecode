446da83cfbe26aefa62ba5e4f86917adecaf1308
David Gleeson <dgleeson1000@yahoo.com>
2012-05-06 00:41:36 +0100

    first Commit

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>SpiceCode.Com</title>
+<style type="text/css">
+<!--
+body {
+	background-color: #52AC0C;
+      /* background-image:url('images/spicecode_background_color.jpg'); */
+
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+#apDiv1 {
+	position:absolute;
+	left:105px;
+	top:0px;
+	width:1030px;
+	height:783px;
+background-image:url('images/spice_code.jpg');
+ background-repeat:no-repeat; 
+ background-position:center top; 
+	
+background-color: #5dca0f;
+	
+}
+#apDiv2 {
+	position:absolute;
+	left:105px;
+	top:2px;
+	width:1030px;
+	height:114px;
+	
+
+}
+#apDiv3 {
+	position:absolute;
+	left:629px;
+	top:97px;
+	width:506px;
+	height:23px;
+	
+
+`	
+}
+-->
+</style>
+<link href="styles.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+<div id="apDiv1"></div>
+
+</body>
+</html>
